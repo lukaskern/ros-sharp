df2c273f1aaa7c82bbac4cf44183e3ae82573151
Lukas Kern <lukas.kern2@gmail.com>
2018-07-30 19:54:15 +0200

    Add message type PointStamped and update dll
    Also update csproj

---
+++ Libraries/RosBridgeClient/Messages/Geometry/PointStamped.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Geometry
+{
+    public class PointStamped : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "geometry_msgs/PointStamped";
+        public Standard.Header header;
+        public Point point;
+        public PointStamped()
+        {
+            header = new Standard.Header();
+            point = new Point();
+        }
+    }
+}
